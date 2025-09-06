8aa9c37fd96b6e81a6a385d5bd6d1319352125df
shahriar00 <shahriarsazid783@gmail.com>
2025-09-06 09:13:13 +0600

    chore: create necessary project folder  and approute

---
+++ PhotosGallery/Approute/Approute.swift
@@ -0,0 +1,8 @@
+//
+//  Approute.swift
+//  PhotosGallery
+//
+//  Created by Shahriar Islam Sazid on 9/6/25.
+//
+
+import Foundation
---
+++ PhotosGallery/View/PhotoGalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoGalleryView.swift
+//  PhotosGallery
+//
+//  Created by Shahriar Islam Sazid on 9/6/25.
+//
+
+import SwiftUI
+
+struct PhotoGalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoGalleryView()
+}
